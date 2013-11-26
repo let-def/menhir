e466de261ad72af8fe693a4cfc6fe1151ccc812f
Frederic Bour <frederic.bour@lakaban.net>
2013-11-26 15:58:06 +0100

    REMOVE ME: local installation settings

--- src/installation.ml
+++ src/installation.ml
@@ -1,16 +1,2 @@
-(**************************************************************************)
-(*                                                                        *)
-(*  Menhir                                                                *)
-(*                                                                        *)
-(*  François Pottier, INRIA Rocquencourt                                  *)
-(*  Yann Régis-Gianas, PPS, Université Paris Diderot                      *)
-(*                                                                        *)
-(*  Copyright 2005-2008 Institut National de Recherche en Informatique    *)
-(*  et en Automatique. All rights reserved. This file is distributed      *)
-(*  under the terms of the Q Public License version 1.0, with the change  *)
-(*  described in file LICENSE.                                            *)
-(*                                                                        *)
-(**************************************************************************)
-
-let libdir = "/home/fpottier/dev/menhir/trunk/src/"
-let ocamlfind = false
+let libdir = "/home/def/Local/tmp/share/menhir"
+let ocamlfind = true
