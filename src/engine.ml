37178918abaad671eb956ced89bc049e2143abb6
Frederic Bour <frederic.bour@lakaban.net>
2014-01-12 13:26:26 +0100

    Purify: remove lexer from Engine

--- src/engine.ml
+++ src/engine.ml
@@ -16,6 +16,10 @@ open EngineTypes
 
 (* The LR parsing engine. *)
 
+let fst3 (a,_,_) = a
+let snd3 (_,a,_) = a
+let thd3 (_,_,a) = a
+
 (* This module is used:
 
    - at compile time, if so requested by the user, via the --interpret options;
@@ -50,10 +54,11 @@ module Make (T : TABLE) = struct
      previous token. If [env.shifted] has not yet reached its limit,
      it is incremented. *)
 
-  let discard env : (state, semantic_value, token) env =
-    let lexbuf = env.lexbuf in
-    let token = env.lexer lexbuf in
-    Log.lookahead_token lexbuf (T.token2terminal token);
+  let discard env token : (state, semantic_value, token) env =
+    Log.lookahead_token
+      (fst3 token)
+      (T.token2terminal (snd3 token))
+      (thd3 token);
     let shifted = env.shifted + 1 in
     let shifted =
       if shifted >= 0
@@ -136,7 +141,7 @@ module Make (T : TABLE) = struct
        current state and the current lookahead token, in order to
        determine which action should be taken. *)
 
-    let token = env.token in
+    let token = snd3 env.token in
     T.action
       env.current                    (* determines a row *)
       (T.token2terminal token)       (* determines a column *)
@@ -163,17 +168,16 @@ module Make (T : TABLE) = struct
 
     Log.shift terminal s';
 
-    let lexbuf = env.lexbuf in
-
     let env =
+      let startp, _, endp = env.token in
       { env with
         (* Push a new cell onto the stack, containing the identity of the
            state that we are leaving. *)
         stack = {
           state = env.current;
           semv = value;
-          startp = lexbuf.Lexing.lex_start_p;
-          endp = lexbuf.Lexing.lex_curr_p;
+          startp;
+          endp;
           next = env.stack;
         };
         (* Switch to state [s']. *)
@@ -237,7 +241,7 @@ module Make (T : TABLE) = struct
   and initiate env : result =
     assert (env.shifted >= 0);
     if T.recovery && env.shifted = 0 then begin
-      Log.discarding_last_token (T.token2terminal env.token);
+      Log.discarding_last_token (T.token2terminal (snd3 env.token));
       `Feed { env; tag = `Feed_error }
     end
     else
@@ -319,12 +323,12 @@ module Make (T : TABLE) = struct
     | T.Accept v -> `Accept v
     | Error      -> `Reject
 
-  let feed s =
+  let feed s token =
     match s.tag with
     | `Feed ->
-      { env = discard s.env; tag = `Step_run }
+      { env = discard s.env token; tag = `Step_run }
     | `Feed_error ->
-      { env = { (discard s.env) with shifted = 0 }; tag = `Step_action }
+      { env = { (discard s.env token) with shifted = 0 }; tag = `Step_action }
 
   (* --------------------------------------------------------------------------- *)
 
@@ -351,16 +355,16 @@ module Make (T : TABLE) = struct
       lexer lexbuf
     in
 
+    let { Lexing.lex_start_p; lex_curr_p } = lexbuf in
+
     (* Log our first lookahead token. *)
 
-    Log.lookahead_token lexbuf (T.token2terminal token);
+    Log.lookahead_token lex_start_p (T.token2terminal token) lex_curr_p;
 
     (* Build an initial environment. *)
 
     let env = {
-      lexer = lexer;
-      lexbuf = lexbuf;
-      token = token;
+      token = (lex_start_p, token, lex_curr_p);
       shifted = max_int;
       previouserror = max_int;
       stack = empty;
@@ -380,7 +384,9 @@ module Make (T : TABLE) = struct
         | `Accept v -> v
         | `Reject -> raise _eRR
         | `Feed p ->
-          aux (step (feed p))
+          let token = lexer lexbuf in
+          let { Lexing.lex_start_p; lex_curr_p } = lexbuf in
+          aux (step (feed p (lex_start_p, token, lex_curr_p)))
       in
       aux (run env)
 
